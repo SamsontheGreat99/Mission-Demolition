23080e092f3d181e997927e5dd0df4e976be9e3a
SamsontheGreat99 <snichols2022@gmail.com>
2022-12-08 11:53:30 +0000

    assets

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    private int score;
+    public Text imTired;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+
+        imTired.text = "Highscore: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
